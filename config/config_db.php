17fa0ea2610a4118c117c4f5f1bbab90cf74f348
aern1234 <arthitaya.j@rmutsvmail.com>
2025-04-11 15:59:37 +0700

    refactor database connection function

--- config/config_db.php
+++ config/config_db.php
@@ -1,9 +1,16 @@
 <?php
-function connect_db()
-{
-    $host = 'localhost';
-    $db = 'room_reservation_db';
+function connect_db() {
+    $host = 'localhost'; // Database host
+    $dbname = 'room_reservation_db';
     $username = 'root';
-    $password = '';
-    return  new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    $password = 'rootroot';
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("DB connection failed: " . $e->getMessage());
+    }
 }
+
